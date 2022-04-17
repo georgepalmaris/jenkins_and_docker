88bdcb63c38ebba4d8b7f95d7191f88746f273f1
georgepalmaris <george.charles.finn@gmail.com>
2022-04-17 22:12:39 +0100

    Implement first reactive component

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # jenkins_and_docker
-For learning and implementing multibranch pipelines
+For learning and implementing multibranch pipelines with jenkins and docker - contains a simple nodejs express backend and react semantic-ui frontend.
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import "./App.css";
-import { Container, Header } from 'semantic-ui-react';
+import './app.css';
+import 'semantic-ui-css/semantic.min.css'
+
+// Components
+import WeatherCard from "./components/weather/weather";
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 function App() {
-    const [lat, setLat] = useState([]);
-    const [long, setLong] = useState([]);
-    const [data, setData] = useState([]);
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
+    const [data, setData] = useState(null);
 
     // Combination of lifecycle hooks to keep components reactive
     useEffect(() => {
         const fetchData = async () => {
-
             // Get the users current long and lat coords
             navigator.geolocation.getCurrentPosition(function(position) {
                 setLat(position.coords.latitude);
                 setLong(position.coords.longitude);
             });
-
-            if (lat.length != 0 && long.length != 0) {
+    
+            if (lat && long) {
                 // Make a request to our express backend
                 axios.get('/getweather', { 
                     params: 
@@ -32,20 +35,25 @@ function App() {
                     console.log(response);
                 });
             }
-        }
+        };
 
         fetchData();
-    }, [lat,long])
+    }, [lat, long])
 
     return (
         // Render the apps components
-        <div className="App">
-            {(typeof data.main != 'undefined') ? (
-                <div><p>Has data</p></div>
-                // <Weather weatherData={data}/>
-            ): (
-                <div><p>No data to display</p></div>
-            )}
+        <div className="app">
+            <header className="app-header">
+                {(data) ? (
+                    <WeatherCard weatherData={data}/>
+                ): (
+                    <div>
+                        <Dimmer active>
+                            <Loader>Loading..</Loader>
+                        </Dimmer>
+                    </div>
+                )}
+            </header>
         </div>
     );
 }
